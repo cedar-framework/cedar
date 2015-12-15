bd443b9825db9cbe0904bc70c5a9ace889b76803
Andrew Reisner <areisne2@illinois.edu>
2015-12-15 10:58:41 -0600

    Uses ndarray for relax_stencil.

--- include/boxmg/2d/relax_stencil.h
+++ include/boxmg/2d/relax_stencil.h
@@ -1,44 +1,53 @@
 #ifndef BOXMG_2D_CORE_RELAX_STENCIL_H
 #define BOXMG_2D_CORE_RELAX_STENCIL_H
 
-#include "boxmg/2d/types.h"
-#include "boxmg/2d/array.h"
+#include <boxmg/2d/types.h>
+#include <boxmg/array.h>
 
 namespace boxmg { namespace bmg2d {
 
 
-class relax_stencil : public Array<len_t, real_t>
+class relax_stencil : public array<len_t, real_t,3>
 {
 public:
 	relax_stencil() {};
 	relax_stencil(len_t nx, len_t ny, unsigned int nghosts=1);
 
-	using Array<len_t,real_t>::index;
+	using array<len_t,real_t,3>::index;
+	using array<len_t,real_t,3>::operator();
 
-	len_t index(len_t i, len_t j, int dir) const
-	{
-		return dir*len_[0]*len_[1] + i*stride_[0] + j * stride_[1];
-	}
+	const virtual range_t<len_t> & range(int i) const;
+	const virtual range_t<len_t> & grange(int i) const;
+	virtual len_t shape(int i) const;
 
-	real_t operator()(len_t i, len_t j, int dir) const
-	{
-		#ifdef DEBUG
-		this->check_bounds(i,j);
-		return data_.at(index(i,j,dir));
-		#else
-		return data_.data()[index(i,j,dir)];
-		#endif
-	}
+	/* len_t index(len_t i, len_t j, int dir) const */
+	/* { */
+	/* 	return dir*len_[0]*len_[1] + i*stride_[0] + j * stride_[1]; */
+	/* } */
 
-	real_t & operator()(len_t i, len_t j, int dir)
-	{
-		#ifdef DEBUG
-		this->check_bounds(i,j);
-		return data_.at(index(i,j,dir));
-		#else
-		return data_.data()[index(i,j,dir)];
-		#endif
-	}
+	/* real_t operator()(len_t i, len_t j, int dir) const */
+	/* { */
+	/* 	#ifdef DEBUG */
+	/* 	this->check_bounds(i,j); */
+	/* 	return data_.at(index(i,j,dir)); */
+	/* 	#else */
+	/* 	return data_.data()[index(i,j,dir)]; */
+	/* 	#endif */
+	/* } */
+
+	/* real_t & operator()(len_t i, len_t j, int dir) */
+	/* { */
+	/* 	#ifdef DEBUG */
+	/* 	this->check_bounds(i,j); */
+	/* 	return data_.at(index(i,j,dir)); */
+	/* 	#else */
+	/* 	return data_.data()[index(i,j,dir)]; */
+	/* 	#endif */
+	/* } */
+private:
+	int num_ghosts;
+	std::array<range_t<len_t>, 2> range_;
+	std::array<range_t<len_t>, 2> grange_;
 };
 
 
--- src/2d/relax_stencil.cc
+++ src/2d/relax_stencil.cc
@@ -2,13 +2,37 @@
 
 using namespace boxmg::bmg2d;
 
-relax_stencil::relax_stencil(len_t nx, len_t ny, unsigned int nghosts) : Array(nx,ny,nghosts,false)
+relax_stencil::relax_stencil(len_t nx, len_t ny, unsigned int nghosts) : array(nx+2*nghosts,ny+nghosts*2,2), num_ghosts(nghosts)
 {
-	len_t size;
-	nx += nghosts*2;
-	ny += nghosts*2;
+	range_[0] = boxmg::range(static_cast<len_t>(nghosts), static_cast<len_t>(nx + nghosts));
+	range_[1] = boxmg::range(static_cast<len_t>(nghosts), static_cast<len_t>(ny + nghosts));
 
-	size = nx*ny*2;
+	grange_[0] = boxmg::range(static_cast<len_t>(0), nx + 2*nghosts);
+	grange_[1] = boxmg::range(static_cast<len_t>(0), ny + 2*nghosts);
+}
 
-	data_.resize(size);
+
+const boxmg::range_t<boxmg::len_t> & relax_stencil::range(int i) const
+{
+		#ifdef DEBUG
+		return range_.at(i);
+		#else
+		return range_[i];
+		#endif
+}
+
+
+const boxmg::range_t<boxmg::len_t> & relax_stencil::grange(int i) const
+{
+	#ifdef DEBUG
+	return grange_.at(i);
+	#else
+	return grange_[i];
+	#endif
+}
+
+
+boxmg::len_t relax_stencil::shape(int i) const
+{
+	return this->len(i) - 2*num_ghosts;
 }
