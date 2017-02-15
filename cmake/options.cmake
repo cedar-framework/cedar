43010432b68b71d20d3b5d19a3d70916afc25e3a
Andrew Reisner <areisne2@illinois.edu>
2017-02-15 12:06:11 -0600

    adds option for building examples

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,3 +1,4 @@
 option(ENABLE_UNIT_TESTS "Enable unit testing" OFF)
 option(ENABLE_MPI "Enable distributed solver" ON)
 option(ENABLE_3D "Enable 3D solver" ON)
+option(ENABLE_EXAMPLES "Build examples" ON)
