960910b009da458b4a5c05c330d09924d9c0949d
Andrew Reisner <areisne2@illinois.edu>
2017-03-01 08:34:10 -0600

    bugfix for cgsolve test

--- test/2d/mpi/test_cgsolve.cc
+++ test/2d/mpi/test_cgsolve.cc
@@ -52,28 +52,33 @@ TEST(MPICGSolver, BoxMG) {
 	auto nx = 400;
 	auto ny = nx;
 
-	auto grid = util::create_topo_global(MPI_COMM_WORLD, nx, ny);
+	auto grid0 = util::create_topo_global(MPI_COMM_WORLD, nx, ny);
+	auto grid1 = util::create_topo_global(MPI_COMM_WORLD, nx, ny);
 
-	auto so0 = mpi::gallery::poisson(grid);
-	auto so1 = mpi::gallery::poisson(grid);
-	mpi::grid_func b(grid);
+	auto so0 = mpi::gallery::poisson(grid0);
+	auto so1 = mpi::gallery::poisson(grid1);
+	mpi::grid_func b0(grid0);
+	mpi::grid_func b1(grid1);
 
-	set_problem(b,
-	            [](real_t x, real_t y) {
-		            const double pi = M_PI;
-		            return 8*(pi*pi)*sin(2*pi*x)*sin(2*pi*y);
-	            });
+	auto rhsf = [](real_t x, real_t y) {
+		const double pi = M_PI;
+		return 8*(pi*pi)*sin(2*pi*x)*sin(2*pi*y);
+	};
+
+
+	set_problem(b0, rhsf);
+	set_problem(b1, rhsf);
 
 	auto conf0 = std::make_shared<config::reader>("test-cgsolve-0.json");
 	auto conf1 = std::make_shared<config::reader>("test-cgsolve-1.json");
 
 	log::init_level(*conf0);
 	mpi::solver bmg0(std::move(so0), conf0);
-	auto sol0 = bmg0.solve(b);
+	auto sol0 = bmg0.solve(b0);
 
 	log::init_level(*conf1);
 	mpi::solver bmg1(std::move(so1), conf1);
-	auto sol1 = bmg1.solve(b);
+	auto sol1 = bmg1.solve(b1);
 
 	auto diff = sol0 - sol1;
 
