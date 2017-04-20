8dbe1d4d0c2900e79ac1b7b319aae24c46d296cd
Andrew Reisner <areisne2@illinois.edu>
2017-04-20 12:39:02 -0500

    Periodic stencil exchange.

--- examples/2d/mpi/per_halo.cc
+++ examples/2d/mpi/per_halo.cc
@@ -5,6 +5,7 @@
 
 #include <cedar/types.h>
 #include <cedar/2d/mpi/grid_func.h>
+#include <cedar/2d/mpi/stencil_op.h>
 #include <cedar/2d/kernel/mpi/factory.h>
 #include <cedar/2d/kernel/mpi/registry.h>
 #include <cedar/2d/util/topo.h>
@@ -13,8 +14,11 @@
 #include <cedar/util/time_log.h>
 
 
-static void draw(const cedar::cdr2::mpi::grid_func & b, std::ostream & os)
+static void draw(const cedar::cdr2::mpi::grid_func & b, std::string prefix)
 {
+	auto & topo = b.grid();
+	std::ofstream os("output/" + prefix + "-gfunc-" + std::to_string(topo.coord(0)) +
+	                 "." + std::to_string(topo.coord(1)));
 	for (auto j : b.grange(1)) {
 		for (auto i : b.grange(0)) {
 			if (b(i,j) < 0)
@@ -24,6 +28,84 @@ static void draw(const cedar::cdr2::mpi::grid_func & b, std::ostream & os)
 			os << " ";
 		}
 		os << '\n';
+	}
+
+	os.close();
+}
+
+
+static void draw_so(const cedar::cdr2::mpi::stencil_op & so, std::string prefix)
+{
+	using namespace cedar;
+	using namespace cedar::cdr2;
+
+	std::vector<std::shared_ptr<std::ofstream>> osv;
+
+	auto format = [](real_t v) -> std::string {
+		if (v < 0.0)
+			return "***";
+		else {
+			auto iv = static_cast<int>(v);
+			std::string ret("");
+			if (iv < 100)
+				ret += "0";
+			if (iv < 10)
+				ret += "0";
+			ret += std::to_string(static_cast<int>(v));
+			return ret;
+		}
+	};
+
+	auto & topo = so.grid();
+	auto & o = so.stencil();
+
+	for (int i = 0; i < 3; ++i) {
+		osv.emplace_back(std::make_shared<std::ofstream>("output/" + prefix + "-stencil-" +
+		                                                 std::to_string(topo.coord(0)) +
+		                                                 "." + std::to_string(topo.coord(1)) +
+		                                                 "-" + std::to_string(i)));
+	}
+
+	for (auto j : o.grange(1)) {
+		for (auto i : o.grange(0)) {
+			*osv[0] << format(o(i,j,dir::C)) << " ";
+			*osv[1] << format(o(i,j,dir::W)) << " ";
+			*osv[2] << format(o(i,j,dir::S)) << " ";
+		}
+		for (int i = 0; i < 3; ++i) *(osv[i]) << '\n';
+	}
+
+	for (int i = 0; i < 3; ++i) osv[i]->close();
+}
+
+
+static void fill_gfunc(cedar::cdr2::mpi::grid_func & b)
+{
+	auto & topo = b.grid();
+	b.set(-1);
+	for (auto j : b.range(1)) {
+		for (auto i : b.range(0)) {
+			b(i,j) = topo.coord(1)*topo.nproc(0) + topo.coord(0);
+		}
+	}
+}
+
+
+static void fill_stencil(cedar::cdr2::mpi::stencil_op & so)
+{
+	using namespace cedar;
+	using namespace cedar::cdr2;
+	auto & o = so.stencil();
+	o.set(-1);
+
+	auto & topo = so.grid();
+
+	for (auto j : o.range(1)) {
+		for (auto i : o.range(0)) {
+			o(i,j,dir::C) = 100*topo.coord(0) + 10*topo.coord(1);
+			o(i,j,dir::W) = 100*topo.coord(0) + 10*topo.coord(1) + 1;
+			o(i,j,dir::S) = 100*topo.coord(0) + 10*topo.coord(1) + 2;
+		}
 	}
 }
 
@@ -46,35 +128,24 @@ int main(int argc, char *argv[])
 
 
 	mpi::grid_func b(grid);
+	mpi::stencil_op so(grid);
 	auto kreg = std::make_shared<cedar::cdr2::kernel::mpi::registry>();
 	cedar::cdr2::kernel::mpi::factory::init(kreg, conf);
 
-	b.set(-1);
 	void *halo_ctx;
 	kreg->halo_setup(*grid, &halo_ctx);
 	b.halo_ctx = halo_ctx;
+	so.halo_ctx = halo_ctx;
 
-	{
-		for (auto j : b.range(1)) {
-			for (auto i : b.range(0)) {
-				b(i,j) = grid->coord(1)*grid->nproc(0) + grid->coord(0);
-			}
-		}
-
-		std::ofstream ofile("output/before-" + std::to_string(grid->coord(0)) +
-		                    "." + std::to_string(grid->coord(1)));
-		draw(b, ofile);
-		ofile.close();
-	}
-
+	fill_gfunc(b);
+	draw(b, "before");
 	kreg->halo_exchange(b);
+	draw(b, "after");
 
-	{
-		std::ofstream ofile("output/after-" + std::to_string(grid->coord(0)) +
-		                    "." + std::to_string(grid->coord(1)));
-		draw(b, ofile);
-		ofile.close();
-	}
+	fill_stencil(so);
+	draw_so(so, "before");
+	kreg->halo_stencil_exchange(so);
+	draw_so(so, "after");
 
 	log::status << "Finished Test" << std::endl;
 
