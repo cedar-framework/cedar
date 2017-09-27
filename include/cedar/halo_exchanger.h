bdac7e21f96cc76df4842c3a835cf85c3781e768
Andrew Reisner <areisne2@illinois.edu>
2017-09-27 10:54:02 -0500

    Simplified dimensional halo exchanger.

--- include/cedar/halo_exchanger.h
+++ include/cedar/halo_exchanger.h
@@ -8,23 +8,10 @@
 
 namespace cedar {
 
-	template<unsigned int nd>
-		struct halo_exchanger_t {
-			typedef void type;
-		};
-
-	template<>
-		struct halo_exchanger_t<2> {
-		typedef cedar::cdr2::mpi::msg_exchanger type;
-	};
-
-	template<>
-		struct halo_exchanger_t<3> {
-		typedef cdr3::mpi::msg_exchanger type;
-	};
-
 	template <unsigned int nd>
-		using halo_exchanger = typename halo_exchanger_t<nd>::type;
+		using halo_exchanger = typename std::conditional<nd==2,
+		cdr2::mpi::msg_exchanger,
+		cdr3::mpi::msg_exchanger>::type;
 }
 
 #endif
