26008f59f730c02d640d2481190b6e6beda9465f
Andrew Reisner <areisne2@illinois.edu>
2017-12-18 13:24:25 -0600

    BMG2_SymStd_SETUP_comm assumes 1-based MPI rank for parent comm.

--- include/cedar/2d/relax/mpi/ml_relax.h
+++ include/cedar/2d/relax/mpi/ml_relax.h
@@ -67,7 +67,7 @@ class ml_line_relaxer
 public:
 	ml_line_relaxer(relax_dir dir) : dir(dir) {}
 
-	void init_ndim(int nproc, int nproc_other, int coord, len_t nlines, len_t nlines_other,
+	void init_ndim(int nproc, int nproc_other, int coord, int coord_other, len_t nlines, len_t nlines_other,
 	               int min_gsz, int nog, MPI_Comm comm)
 	{
 			int workspace_size, nspace;
@@ -75,10 +75,9 @@ public:
 			                               &nspace, &this->nlevels);
 			comms.init(2, this->nlevels - 1);
 			MPI_Comm gcomm;
-			MPI_Comm_split(comm, nproc_other, nproc,
-			               &gcomm);
+			MPI_Comm_split(comm, coord_other, coord, &gcomm);
 			comms(0, this->nlevels - 2) = MPI_Comm_c2f(gcomm);
-			MPI_BMG2_SymStd_SETUP_comm(comms.data(), this->nlevels, coord, min_gsz);
+			MPI_BMG2_SymStd_SETUP_comm(comms.data(), this->nlevels, coord+1, min_gsz);
 			sor_ptrs.init(this->nlevels);
 			BMG2_SymStd_SETUP_ADD_SOR_PTRS(nproc, nlines, min_gsz, this->nlevels,
 			                               &workspace_size, sor_ptrs.data());
@@ -104,12 +103,14 @@ public:
 		int min_gsz = 3;
 
 		if (dir == relax_dir::x) {
-			init_ndim(topo.nproc(0), topo.nproc(1), topo.coord(0),
+			init_ndim(topo.nproc(0), topo.nproc(1),
+			          topo.coord(0), topo.coord(1),
 			          topo.nlocal(0) - 2, topo.nlocal(1) - 2,
 			          min_gsz, topo.nlevel(), topo.comm);
 			MPI_BMG2_SymStd_SETUP_lines_x(sod.data(), sor.data(), so.len(0), so.len(1), nstencil);
 		} else {
-			init_ndim(topo.nproc(1), topo.nproc(0), topo.coord(1),
+			init_ndim(topo.nproc(1), topo.nproc(0),
+			          topo.coord(1), topo.coord(0),
 			          topo.nlocal(1) - 2, topo.nlocal(0) - 2,
 			          min_gsz, topo.nlevel(), topo.comm);
 			MPI_BMG2_SymStd_SETUP_lines_y(sod.data(), sor.data(), so.len(0), so.len(1), nstencil);
