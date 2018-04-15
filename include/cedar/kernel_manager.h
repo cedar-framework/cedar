0cbba09a02504b4c2aad1eb18474fd949c325edb
Andrew Reisner <areisne2@illinois.edu>
2018-04-15 16:56:04 -0500

    Move type mapping logic outside kernel_manager.

--- include/cedar/kernel_manager.h
+++ include/cedar/kernel_manager.h
@@ -1,10 +1,6 @@
 #ifndef CEDAR_KERNEL_MANAGER_H
 #define CEDAR_KERNEL_MANAGER_H
 
-#include <memory>
-#include <map>
-#include <tuple>
-
 #include <cedar/type_list.h>
 #include <cedar/kernel_params.h>
 #include <cedar/config/reader.h>
@@ -12,88 +8,19 @@
 
 namespace cedar {
 
-template<class T1, class T2>
-struct same_type
-{
-	static const bool value = false;
-};
-
-template<class T>
-struct same_type<T, T>
-{
-	static const bool value = true;
-};
-
-
-template<class U>
-class kernel_manager;
-
-template<typename... types>
-class kernel_manager<type_list<types...>>
+template<class tlist>
+class kernel_manager : public type_map<tlist>
 {
 public:
+	using parent = type_map<tlist>;
+
     kernel_manager(std::shared_ptr<kernel_params> params) : params(params) {}
 	kernel_manager(config::reader & conf) { params = build_kernel_params(conf); }
-
-	typedef std::tuple<std::map<std::string,
-	                            std::shared_ptr<types>>...> mtype;
-	mtype kerns;
-	std::array<std::string, std::tuple_size<mtype>::value> defaults;
-
-	template<int n, class T>
-	struct map_of_type: same_type<std::shared_ptr<T>,
-	                              typename std::tuple_element<n, mtype>::type::mapped_type>
-	{};
-
-
-	template<int n, class T, bool match = false>
-	struct matching_index
-	{
-		static const std::size_t value = matching_index<n+1, T, map_of_type<n+1, T>::value>::value;
-	};
-
-	template<int n, class T>
-	struct matching_index<n, T, true>
-	{
-		static const std::size_t value = n;
-	};
-
-	template<class T>
-	std::shared_ptr<T> get_ptr()
-	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		return std::get<i>(kerns)[defaults[i]];
-	}
-
-
-	template<class T>
-	std::shared_ptr<T> get_ptr(const std::string & name)
-	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		return std::get<i>(kerns)[name];
-	}
-
-	template<class T>
-	T & get()
-	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		return *std::get<i>(kerns)[defaults[i]];
-	}
-
-
-	template<class T>
-	T & get(const std::string & name)
-	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		return *std::get<i>(kerns)[name];
-	}
-
 
 	template<class T, class rclass>
 	void add(const std::string & name)
 	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		std::get<i>(kerns)[name] = std::make_shared<rclass>();
+		parent::template add<T, rclass>(name);
 		this->init<T>(name);
 	}
 
@@ -101,24 +28,15 @@ public:
 	template<class T, class rclass, class... Args>
 	void add(const std::string & name, Args&&... args)
 	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		std::get<i>(kerns)[name] = std::make_shared<rclass>(std::forward<Args>(args)...);
+		parent::template add<T, rclass>(name, std::forward<Args>(args)...);
 		this->init<T>(name);
-	}
-
-
-	template<class T>
-	void set(const std::string & name)
-	{
-		const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
-		defaults[i] = name;
 	}
 
 
 	template<class T, class... Args>
 	void setup(Args&&... args)
 	{
-		auto & kern = this->get<T>();
+		auto & kern = parent::template get<T>();
 		log::debug << "setup kernel <" << kern.name << ">" << std::endl;
 		kern.setup(std::forward<Args>(args)...);
 	}
@@ -126,7 +44,7 @@ public:
 	template<class T, class... Args>
 	void run(Args&&... args)
 	{
-		auto & kern = this->get<T>();
+		auto & kern = parent::template get<T>();
 		log::debug << "running kernel <" << kern.name << ">" << std::endl;
 		kern.run(std::forward<Args>(args)...);
 	}
@@ -135,7 +53,7 @@ public:
 	template<class T>
 	void init(const std::string & name)
 	{
-		this->get<T>(name).add_params(params);
+		parent::template get<T>(name).add_params(params);
 	}
 
 protected:
--- include/cedar/type_list.h
+++ include/cedar/type_list.h
@@ -1,6 +1,10 @@
 #ifndef CEDAR_TYPE_LIST_H
 #define CEDAR_TYPE_LIST_H
 
+#include <memory>
+#include <map>
+#include <tuple>
+
 namespace cedar
 {
 	template<class... T> struct type_list {};
@@ -11,6 +15,105 @@ namespace cedar
 	template<typename ... a, typename ... b>
 	struct type_cat<type_list<a ...>, type_list<b ...>>
 	{ typedef type_list<a ..., b ...> type; };
+
+
+	template<class T1, class T2>
+	struct same_type
+	{
+		static const bool value = false;
+	};
+
+	template<class T>
+	struct same_type<T, T>
+	{
+		static const bool value = true;
+	};
+
+
+	template<class U>
+	class type_map;
+
+	template<typename... types>
+	class type_map<type_list<types...>>
+	{
+	public:
+		typedef std::tuple<std::map<std::string,
+		                            std::shared_ptr<types>>...> mtype;
+		mtype kerns;
+		std::array<std::string, std::tuple_size<mtype>::value> defaults;
+
+		template<int n, class T>
+		struct map_of_type: same_type<std::shared_ptr<T>,
+		                              typename std::tuple_element<n, mtype>::type::mapped_type>
+		{};
+
+
+		template<int n, class T, bool match = false>
+		struct matching_index
+		{
+			static const std::size_t value = matching_index<n+1, T, map_of_type<n+1, T>::value>::value;
+		};
+
+		template<int n, class T>
+		struct matching_index<n, T, true>
+		{
+			static const std::size_t value = n;
+		};
+
+		template<class T>
+		std::shared_ptr<T> get_ptr()
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			return std::get<i>(kerns)[defaults[i]];
+		}
+
+
+		template<class T>
+		std::shared_ptr<T> get_ptr(const std::string & name)
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			return std::get<i>(kerns)[name];
+		}
+
+		template<class T>
+		T & get()
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			return *std::get<i>(kerns)[defaults[i]];
+		}
+
+
+		template<class T>
+		T & get(const std::string & name)
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			return *std::get<i>(kerns)[name];
+		}
+
+
+		template<class T, class rclass>
+		void add(const std::string & name)
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			std::get<i>(kerns)[name] = std::make_shared<rclass>();
+		}
+
+
+		template<class T, class rclass, class... Args>
+		void add(const std::string & name, Args&&... args)
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			std::get<i>(kerns)[name] = std::make_shared<rclass>(std::forward<Args>(args)...);
+		}
+
+
+		template<class T>
+		void set(const std::string & name)
+		{
+			const std::size_t i = matching_index<0, T, map_of_type<0, T>::value>::value;
+			defaults[i] = name;
+		}
+	};
 }
 
 #endif
