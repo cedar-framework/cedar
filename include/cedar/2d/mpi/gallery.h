12d2fe60b627c9116e8e1238c4303a6876000d4e
Andrew Reisner <areisne2@illinois.edu>
2018-06-14 11:03:15 -0500

    Add nine point 2D MPI example.

--- include/cedar/2d/mpi/gallery.h
+++ include/cedar/2d/mpi/gallery.h
@@ -7,6 +7,7 @@ namespace cedar { namespace cdr2 { namespace mpi { namespace gallery {
 
 stencil_op<five_pt> poisson(topo_ptr grid);
 stencil_op<five_pt> diag_diffusion(topo_ptr grid, real_t dx, real_t dy);
+stencil_op<nine_pt> fe(topo_ptr grid);
 
 }}}}
 
