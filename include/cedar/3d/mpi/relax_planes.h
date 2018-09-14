8db2007e9022f94348ddf62836cf372af9a08394
Andrew Reisner <areisne2@illinois.edu>
2018-09-14 16:29:58 -0500

    Bugfix for plane relaxation with > 2 3D levels.
    The plane_team data structure used to coordinate plane services for
    planes of the same color was being overwritten when used inside a
    multilevel solve.  This simply puts the data structure inside a vector
    for each level.

--- include/cedar/3d/mpi/relax_planes.h
+++ include/cedar/3d/mpi/relax_planes.h
@@ -231,14 +231,16 @@ public:
 
 	void setup(const stencil_op<seven_pt> & so) override
 	{
-		this->setup_impl(so, fine_planes, fine_threads);
+		level_teams.emplace_back();
+		this->setup_impl(so, fine_planes, fine_threads, level_teams.back());
 	}
 	void setup(const stencil_op<xxvii_pt> & so) override
 	{
 		level_threads.emplace_back();
 		level_planes.emplace_back();
+		level_teams.emplace_back();
 		level_map[so.shape(0)] = level_planes.size() - 1;
-		this->setup_impl(so, level_planes.back(), level_threads.back());
+		this->setup_impl(so, level_planes.back(), level_threads.back(), level_teams.back());
 	}
 	void run(const stencil_op<seven_pt> & so, grid_func & x,
 	         const grid_func & b, cycle::Dir cycle_dir) override { this->run_impl(so, x, b, cycle_dir); }
@@ -247,7 +249,8 @@ public:
 
 	template<class sten3, class sten2>
 	void setup_impl(const stencil_op<sten3> & so, std::vector<slv2_ptr<sten2>> & planes,
-	                std::array<plane_ult<sten2>, 2> & threads)
+	                std::array<plane_ult<sten2>, 2> & threads,
+	                std::array<plane_team, 2> & teams)
 	{
 		#ifdef PLANE_AGG
 		this->aggregate = this->params->plane_agg;
@@ -360,7 +363,7 @@ protected:
 	std::vector<std::array<plane_ult<cdr2::nine_pt>, 2>> level_threads;
 	std::array<plane_ult<cdr2::five_pt>, 2> fine_threads;
 	std::map<len_t, std::size_t> level_map;
-	std::array<plane_team, 2> teams;
+	std::vector<std::array<plane_team, 2>> level_teams;
 	bool aggregate;
 
 	std::shared_ptr<grid_topo> slice_topo(const grid_topo & topo3)
