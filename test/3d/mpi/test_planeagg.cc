dc7f15e0b63d8ec0cb8b3f1ae150369192483819
Andrew Reisner <areisne2@illinois.edu>
2018-12-13 14:34:59 -0600

    Stricter tests for plane aggregation.

--- test/3d/mpi/test_planeagg.cc
+++ test/3d/mpi/test_planeagg.cc
@@ -70,14 +70,14 @@ cedar::cdr2::mpi::stencil_op<cedar::cdr2::nine_pt> create_op(cedar::topo_ptr gri
 }
 
 
-static void assert_tol(std::vector<cedar::real_t> & x0,
-                       std::vector<cedar::real_t> & x1)
+static void assert_same(std::vector<cedar::real_t> & x0,
+                        std::vector<cedar::real_t> & x1)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	double tol = 1e-10;
 	for (std::size_t i = 0; i < x0.size(); i++) {
-		ASSERT_LT(std::abs(x0[i] - x1[i]), tol);
+		ASSERT_EQ(std::abs(x0[i] - x1[i]), 0);
 	}
 }
 
@@ -88,11 +88,11 @@ TEST(MPIPlaneAgg, LineRelax5) {
 
 	auto xnoagg = line_agg<relax_dir::x, five_pt>(false, 31, 33, 11);
 	auto xagg   = line_agg<relax_dir::x, five_pt>(true,  31, 33, 11);
-	assert_tol(xnoagg, xagg);
+	assert_same(xnoagg, xagg);
 
 	auto ynoagg = line_agg<relax_dir::y, five_pt>(false, 29, 28, 11);
 	auto yagg   = line_agg<relax_dir::y, five_pt>(true,  29, 28, 11);
-	assert_tol(ynoagg, yagg);
+	assert_same(ynoagg, yagg);
 }
 
 
@@ -102,11 +102,11 @@ TEST(MPIPlaneAgg, LineRelax9) {
 
 	auto xnoagg = line_agg<relax_dir::x, nine_pt>(false, 31, 33, 10);
 	auto xagg   = line_agg<relax_dir::x, nine_pt>(true,  31, 33, 10);
-	assert_tol(xnoagg, xagg);
+	assert_same(xnoagg, xagg);
 
 	auto ynoagg = line_agg<relax_dir::y, nine_pt>(false, 29, 28, 11);
 	auto yagg   = line_agg<relax_dir::y, nine_pt>(true,  29, 28, 11);
-	assert_tol(ynoagg, yagg);
+	assert_same(ynoagg, yagg);
 }
 
 
@@ -117,7 +117,7 @@ TEST(MPIPlaneAgg, PlaneXY7) {
 	auto agg   = plane_agg<relax_dir::xy, seven_pt>(true,  24, 23, 13);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
 
 
@@ -128,7 +128,7 @@ TEST(MPIPlaneAgg, PlaneXZ7) {
 	auto agg   = plane_agg<relax_dir::xz, seven_pt>(true,  24, 21, 10);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
 
 
@@ -139,7 +139,7 @@ TEST(MPIPlaneAgg, PlaneYZ7) {
 	auto agg   = plane_agg<relax_dir::yz, seven_pt>(true,  14, 17, 11);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
 
 
@@ -150,7 +150,7 @@ TEST(MPIPlaneAgg, PlaneXY27) {
 	auto agg   = plane_agg<relax_dir::xy, xxvii_pt>(true,  25, 23, 13);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
 
 
@@ -161,7 +161,7 @@ TEST(MPIPlaneAgg, PlaneXZ27) {
 	auto agg   = plane_agg<relax_dir::xz, xxvii_pt>(true,  24, 21, 10);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
 
 
@@ -172,5 +172,5 @@ TEST(MPIPlaneAgg, PlaneYZ27) {
 	auto agg   = plane_agg<relax_dir::yz, xxvii_pt>(true,  14, 17, 11);
 
 	auto diff = noagg - agg;
-	ASSERT_LT(std::abs(diff.inf_norm()), 1e-10);
+	ASSERT_EQ(std::abs(diff.inf_norm()), 0);
 }
