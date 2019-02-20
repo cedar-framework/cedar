98c56f7f8c87e32a31dc0cd2dac03dc79b4c6d42
Andrew Reisner <areisne2@illinois.edu>
2019-02-20 15:24:53 -0600

    Update test driver to print residual norm.

--- app/relax.cc
+++ app/relax.cc
@@ -1,9 +1,32 @@
 #include <mpi.h>
+#include <random>
+#include <limits>
 
 #include <cedar/3d/util/topo.h>
 #include <cedar/3d/mpi/gallery.h>
 #include <cedar/3d/mpi/kernel_manager.h>
 #include <cedar/cycle/types.h>
+
+
+static void set_random(cedar::cdr3::mpi::grid_func & x)
+{
+	using namespace cedar;
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	std::mt19937 gen;
+	gen.seed(rank);
+	std::uniform_real_distribution<real_t> dis;
+
+	for (auto k : x.range(2)) {
+		for (auto j : x.range(1)) {
+			for (auto i : x.range(0)) {
+				x(i,j,k) = dis(gen);
+			}
+		}
+	}
+}
 
 
 int main(int argc, char *argv[])
@@ -21,8 +44,11 @@ int main(int argc, char *argv[])
 	auto topo = util::create_topo(conf);
 
 	auto so = mpi::gallery::fe(topo);
-	auto b = mpi::grid_func::zeros(topo);
-	auto x = mpi::grid_func::ones(topo);
+
+	auto b = mpi::grid_func::ones(topo);
+	auto x = mpi::grid_func::zeros(topo);
+
+	set_random(x);
 
 	// setup halo
 	{
@@ -42,6 +68,16 @@ int main(int argc, char *argv[])
 		(void)i;
 		kreg->run<mpi::point_relax>(so, x, b, sor, cycle::Dir::DOWN);
 	}
+
+	auto res = mpi::grid_func::zeros(topo);
+	kreg->run<mpi::residual>(so, x, b, res);
+
+	real_t nrm = res.lp_norm<2>();
+	std::cout.precision(std::numeric_limits<real_t>::max_digits10);
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+		std::cout << "residual norm: " << nrm << std::endl;
 
 	MPI_Finalize();
 	return 0;
